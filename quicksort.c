1de61d2777113a00efad58a26a8fdc0d86d5815a
xwk1246_desk <xwk1246@csie.io>
2021-04-25 04:39:57 +0800

    fix quick sort bug

--- quicksort.c
+++ quicksort.c
@@ -5,7 +5,7 @@ void quicksort(int number[], int left, int right) {
     int tmp;
     int pivot = number[right];
     int current = 0;
-    if (j - i > 0) {
+    if (j - i > 0 && i >= 0) {
         while (i < j) {
             while (number[i] <= pivot && i < j) {
                 i++;
@@ -14,14 +14,13 @@ void quicksort(int number[], int left, int right) {
                 j--;
             }
             tmp = number[i];
-            number[i] = number[right];
-            number[right] = tmp;
-            /*
-            for (current = 0; current <= right; current++) {
-                printf("%d", number[current]);
-            }
-            */
+            number[i] = number[j];
+            number[j] = tmp;
+
         }
+        tmp = number[i];
+        number[i] = number[right];
+        number[right] = tmp;
         quicksort(number, left, i - 1);
         quicksort(number, i + 1, right);
     }
